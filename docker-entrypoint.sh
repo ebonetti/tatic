5aac70f121346940876e42b2a1829ac0c5894a51
Enrico Bonetti Vieno <entanglement.ebv@gmail.com>
2019-05-29 16:46:46 +0200

    Improved configuration generation

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
-set -eux;
+set -Eeuo pipefail 
 
-export  example_com=$DOMAIN example__com=${DOMAIN//./\\.};
-envsubst '$example_com,$example__com' < /etc/nginx/conf.d/templates/static.conf > /etc/nginx/conf.d/default.conf;
+#If default configuration server doesn't already exist, create it from static template.
+FILE=/etc/nginx/conf.d/default.conf
+if [ ! -f "$FILE" ]; then
+    if [ -z "$DOMAIN" ]
+    then
+        echo "No domain specified, use the DOMAIN variable: docker run -e DOMAIN=example.com ...";
+        exit;
+    fi
+    export  example_com=$DOMAIN example__com=${DOMAIN//./\\.} fallback=${FALLBACK:-"www"};
+    envsubst '$example_com,$example__com,$fallback' < /etc/nginx/conf.d/templates/static.conf > $FILE;
+fi
 
 exec "$@"
