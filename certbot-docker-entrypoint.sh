5606d648b4a99f24158c884aa09490abf9d07cbe
Enrico Bonetti Vieno <entanglement.ebv@gmail.com>
2019-06-03 12:46:32 +0200

    Cleaned up code

--- certbot-docker-entrypoint.sh
+++ certbot-docker-entrypoint.sh
@@ -3,16 +3,35 @@ set -Eeuo pipefail
 
 trap "exit 1;" SIGINT SIGTERM
 
-#DUMMY certbot docker entrypoint for debugging purposes
-cd /etc/letsencrypt/live/${DOMAIN}/
-rm * || true;
+CERT=/etc/letsencrypt/live/${DOMAIN}/;
+rm -fr $CERT/*#JUST FOR TESTING#####################################################################################
+cd $CERT;
 
-sleep 10;
-echo "Adding Dummy stuff"
-cd /etc/letsencrypt/live/${DOMAIN}/
-openssl req -x509 -nodes -newkey rsa:1024 -days 1 -keyout 'privkey.pem' -out 'fullchain.pem' -subj '/CN=localhost';
-curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/ssl-dhparams.pem > ssl-dhparams.pem;
-mkdir -p /var/www/.well-known/acme-challenge/;
-curl -s https://www.example.com > /var/www/.well-known/acme-challenge/index.html
+if [ ! -f 'fullchain.pem' ] || [ ! -f 'privkey.pem' ]; then
+    echo 'Adding dummy certificates for starting NGINX';
+    openssl req -x509 -nodes -newkey rsa:1024 -days 1\
+     -keyout 'privkey.pem' -out 'fullchain.pem' -subj '/CN=localhost';
+fi;
+if [ ! -f "ssl-dhparams.pem" ] ; then
+    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/ssl-dhparams.pem > ssl-dhparams.pem;
+fi;
 
-exec "$@"
+#If some command is specified execute it, ignore the rest
+if [ -n '"$@"' ]; then
+    exec "$@";
+fi;
+
+ACME=/var/www-acme-challenge/;
+rm -fr $ACME/*;
+
+#Wait for a stable set of subdomains
+while [ $(inotifywait -t 5 -q -e move -e move_self -e create /var/www > /dev/null 2>1; echo $?) != 2 ]; do :; done;
+
+#Ignore eventual subdomains
+#expand
+#on fail new
+mkdir -p $ACME/.well-known/acme-challenge;
+curl -s https://www.example.com > $ACME/.well-known/acme-challenge/index.html;
+
+#Exit on changes to website data or timeout
+inotifywait -t 43200 -q -e move -e move_self -e create /var/www;
