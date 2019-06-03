715a98bc3c16538abc4fdc5064d855975454fb59
Enrico Bonetti Vieno <entanglement.ebv@gmail.com>
2019-06-03 14:29:42 +0200

    Testing a real certificate

--- certbot-docker-entrypoint.sh
+++ certbot-docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -Eeuo pipefail
 trap "exit 1;" SIGINT SIGTERM
 
 CERT=/etc/letsencrypt/live/${DOMAIN}/;
-rm -fr $CERT/*#JUST FOR TESTING#####################################################################################
+rm -fr $CERT/* #JUST FOR TESTING#####################################################################################
 cd $CERT;
 
 if [ ! -f 'fullchain.pem' ] || [ ! -f 'privkey.pem' ]; then
@@ -25,18 +25,20 @@ ACME=/var/www-acme-challenge/;
 rm -fr $ACME/*;
 
 #Wait for a stable set of subdomains
-while [ $(inotifywait -t 5 -q -e move -e move_self -e create /var/www > /dev/null 2>1; echo $?) != 2 ]; do :; done;
+#while [ $(inotifywait -t 5 -q -e move -e move_self -e create /var/www/ > /dev/null 2>1; echo $?) != 2 ]; do :; done;
 
 #Ignore eventual subdomains not connected
-#expand on an existing certificate
+#expand on an existing certificate --expand
 #if everithing else fails request a new certificate
 echo "Started certbot";
-certbot certonly --webroot -w /var/www-acme-challenge/ \
-    --rsa-key-size 4096 --agree-tos --force-renewal \
+certbot certonly --non-interactive --agree-tos \
+    --force-renewal --rsa-key-size 4096 \
+    --webroot -w /var/www-acme-challenge/ \
     --cert-name $DOMAIN --email $EMAIL \
-    -d www.$DOMAIN \
-    --staging;
+    -d www.$DOMAIN;
+    #--staging;
 echo;
 
 #Exit on changes to website data or timeout
-inotifywait -t 43200 -q -e move -e move_self -e create /var/www;
+#inotifywait -t 43200 -e move -e move_self -e create /var/www/;
+sleep 43200;
